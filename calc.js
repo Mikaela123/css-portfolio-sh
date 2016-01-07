8ecaabba140d6459b475601590250036a671f911
mikaela123 <mconsunji.sh@mysummitps.org>
2016-01-07 16:40:21 +0000

    update

--- calc.js
+++ calc.js
@@ -6,6 +6,7 @@ var display = document.getElementById('display');
 var dropdown = document.getElementById("dropdown");
 
 button.addEventListener("click", doMath);
+//This function is for the operations in the calculator. I have made if statements for addition, subtraction, multipication, division, exponents, and finding the square root of a number. I used input1.value and input2.value to distinguish between the two numbers that are going to be in the equation.
 function doMath() {
     if  (dropdown.value == "+"){
         answer = parseInt(input1.value) + parseInt(input2.value);
