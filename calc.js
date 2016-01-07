b72a86e5377a22190cd34643de568aadd104290b
mikaela123 <mconsunji.sh@mysummitps.org>
2016-01-07 23:19:16 +0000

    update

--- calc.js
+++ calc.js
@@ -1,3 +1,5 @@
+/*The var statements help me know which phrases to use type out in
+my "if" statements for my calculator.*/
 var button = document.getElementById("submitButton");
 var input1 = document.getElementById('input1');
 var input2 = document.getElementById('input2');
@@ -6,7 +8,12 @@ var display = document.getElementById('display');
 var dropdown = document.getElementById("dropdown");
 
 button.addEventListener("click", doMath);
-//This function is for the operations in the calculator. I have made if statements for addition, subtraction, multipication, division, exponents, and finding the square root of a number. I used input1.value and input2.value to distinguish between the two numbers that are going to be in the equation.
+/*This function is for the operations in the calculator. I have made "if"
+statements for addition, subtraction, multipication, division, 
+exponents, and square root of a number. I used input1.value and 
+input2.value to distinguish between the two numbers that are going 
+to be in the equation. (input1.value is the firstbox in my calculator 
+and input2.value is the secondbox in my calculator.)*/
 function doMath() {
     if  (dropdown.value == "+"){
         answer = parseInt(input1.value) + parseInt(input2.value);
--- info.js
+++ info.js
@@ -1,8 +1,10 @@
 // JavaScript File
 var item1 = document.getElementById("item1");
 var item2 = document.getElementById("item2");
+var item3 = document.getElementById("item3");
 var info1 = document.getElementById("info1");
 var info2 = document.getElementById("info2");
+var info3 = document.getElementById("info");
 
 item1.addEventListener("click", function(){
     info1.innerHTML = "Some stuff";
@@ -10,6 +12,11 @@ item1.addEventListener("click", function(){
 });
                        
 item2.addEventListener("click", function(){
+    info1.innerHTML = "Item 2 stuff";
+    info2.innerHTML = "Item 2 other stuff";
+});
+
+item3.addEventListener("click", function(){
     info1.innerHTML = "Item 2 stuff";
     info2.innerHTML = "Item 2 other stuff";
 });
