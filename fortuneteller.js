dc173e6b7d23f0990ae4b54ef96c0f802a70f7da
mikaela123 <mconsunji.sh@mysummitps.org>
2016-01-14 23:04:13 +0000

    update

--- fortuneteller.js
+++ fortuneteller.js
@@ -1,37 +1,72 @@
 // JavaScript File
+var fortuneButton = document.getElementsById("fortuneButton");
+var fortuneButtonInfo = document.getElementsById("fortuneButtonInfo");
+
 var pickedColor;
+var hasColor = false
 var colors = document.querySelectorAll("input.colors")
 for(var i = 0; i < colors.length; i++){
     colors[i].addEventListener("click", function (evt){
-        
-        evt.target.style.color = "white"
+
+    if(!hasColor){
+        pickedColor = evt.target.value;       
+        evt.target.style.border = "solid black 2px"
+        hasColor = true
+    }
+
     });
 }
-if()
 
 var pickedNumber;
+var hasNumber = false
 var numbers = document.querySelectorAll("input.numbers")
-for(var i = 0; i < colors.length; i++){
+for(var i = 0; i < numbers.length; i++){
     numbers[i].addEventListener("click", function (evt){
         
-        evt.target.style.color = "white"
+    if(!hasNumber){
+        pickedNumber = evt.target.value;
+        evt.target.style.border = "solid black 2px"
+        hasNumber = true
+    }
+    
     });
 }
 
 var pickedShape;
+var hasShape = false
 var shapes = document.querySelectorAll("button.shapes")
 for(var i = 0; i < shapes.length; i++){
     shapes[i].addEventListener("click", function (evt){
         
-        evt.target.style.color = "white"
+    if(!hasShape){
+        pickedShape = evt.target.value;
+        evt.target.style.border = "solid black 2px"
+        hasShape = true
+    }
+    
     });
 }
 
 var pickedMonth;
+var hasMonth = false
 var months = document.querySelectorAll("input.months")
 for(var i = 0; i < months.length; i++){
     months[i].addEventListener("click", function (evt){
         
-        evt.target.style.color = "white"
+    if(!hasMonth){
+        pickedMonth = evt.target.value;
+        evt.target.style.border = "solid black 2px"
+        hasMonth = true
+    }
+    
+    });
+}
+
+var fortuneButton;
+var fortuneButton = document.querySelectorAll("input.fortuneButton")
+for(var i = 0; i < months.length; i++){
+    fortuneButton[i].addEventListener("click", function (evt){
+        
+        evt.target.style.color = "pink"
     });
 }
--- info.js
+++ info.js
@@ -9,21 +9,18 @@ var newyorkinfo = document.getElementById("newyorkinfo");
 var barcelonainfo = document.getElementById("barcelonainfo");
 
 greece.addEventListener("click", function(){
-    greeceinfo.innerHTML = "Location: Southeastern Europe<br>Capital: Athens<br>Popuation: 11.03million<br>Activities: Beaches, Hiking, Sightseeing, Museums, Boating, Swimming";
+    greeceinfo.innerHTML = "Location: Southeastern Europe<br>Capital: Athens<br>Popuation: 11.03million<br>Activities: Beaches, Hiking, Sightseeing, Museums, Boating, Swimming"
 });
             
 israel.addEventListener("click", function(){
-    israelinfo.innerHTML = "Location: Middle Eastern city west of the Dead Sea<br>Capital: Jerusalem<br>Popuation: 809,112<br>Activities: ";
-    info2.innerHTML = "Item 2 other stuff";
+    israelinfo.innerHTML = "Location: Middle Eastern city west of the Dead Sea<br>Capital: Jerusalem<br>Popuation: 809,112<br>Activities: Cycling, Jeep Tours, ATVs &Quad Runners, Horseback Riding, Rafting & Kayaking, Paragliding, Scuba Diving, Swim with Dolphins, Camel Riding"
 });
 
 newyork.addEventListener("click", function(){
-    newyorkinfo.innerHTML = "Location: Eastern part of the US<br>Capital: New York City<br>Popuation: 8.406 million<br>Activities: ";
-    info2.innerHTML = "Item 2 other stuff";
+    newyorkinfo.innerHTML = "Location: Eastern part of the US<br>Capital: New York City<br>Popuation: 8.406 million<br>Activities: Tour New York, Try all kinds of different cultural food, Walk in Central Park, Broadway Shows, Sightseeing, Museums, Shopping"
 });
 
 barcelona.addEventListener("click", function(){
-    barcelonainfo.innerHTML = "Location: Europe’s Iberian Peninsula<br>Capital: Madrid<br>Popuation: 46.77 million<br>Activities: ";
-    info2.innerHTML = "Item 2 other stuff";
+    barcelonainfo.innerHTML = "Location: Europe's Iberian Peninsula<br>Capital: Madrid<br>Popuation: 46.77 million<br>Activities: Sightseeing, Tours, Boating, Wine Tasting, Concerts, Shopping, Water Sports, Amusment Parkd, Spas"
 });
 
