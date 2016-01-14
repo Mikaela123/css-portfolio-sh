ab215042285924096a51070e4cc2c99515d99b64
mikaela123 <mconsunji.sh@mysummitps.org>
2016-01-14 04:00:18 +0000

    update

--- fortuneteller.js
+++ fortuneteller.js
@@ -1,20 +1,37 @@
 // JavaScript File
-var january = document.getElementById("january");
-var february = document.getElementById("february");
-var march = document.getElementById("march");
-var april = document.getElementById("april");
-var may = document.getElementById("may");
-var june = document.getElementById("june");
-var july = document.getElementById("july");
-var august = document.getElementById("august");
-var september = document.getElementById("september");
-var october = document.getElementById("october");
-var november = document.getElementById("november");
-var december = document.getElementById("december");
-var januaryinfo = document.getElementById("januaryinfo");
+var pickedColor;
+var colors = document.querySelectorAll("input.colors")
+for(var i = 0; i < colors.length; i++){
+    colors[i].addEventListener("click", function (evt){
+        
+        evt.target.style.color = "white"
+    });
+}
+if()
 
+var pickedNumber;
+var numbers = document.querySelectorAll("input.numbers")
+for(var i = 0; i < colors.length; i++){
+    numbers[i].addEventListener("click", function (evt){
+        
+        evt.target.style.color = "white"
+    });
+}
 
-january.addEventListener("click", function(){
-    januaryinfo.innerHTML = "You will win the lottery!";
-});
+var pickedShape;
+var shapes = document.querySelectorAll("button.shapes")
+for(var i = 0; i < shapes.length; i++){
+    shapes[i].addEventListener("click", function (evt){
+        
+        evt.target.style.color = "white"
+    });
+}
 
+var pickedMonth;
+var months = document.querySelectorAll("input.months")
+for(var i = 0; i < months.length; i++){
+    months[i].addEventListener("click", function (evt){
+        
+        evt.target.style.color = "white"
+    });
+}
