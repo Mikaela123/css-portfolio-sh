3eb2c2149110ab7702214a1d4a175ac467c49ccd
mikaela123 <mconsunji.sh@mysummitps.org>
2016-03-18 20:56:30 +0000

    update

--- info.js
+++ info.js
@@ -1,25 +1,30 @@
 // JavaScript File
-var greece = document.getElementById("greece");
-var israel = document.getElementById("israel");
-var newyork = document.getElementById("newyork");
-var barcelona = document.getElementById("barcelona");
-var greeceinfo = document.getElementById("greeceinfo");
-var israelinfo = document.getElementById("israelinfo");
-var newyorkinfo = document.getElementById("newyorkinfo");
-var barcelonainfo = document.getElementById("barcelonainfo");
+var item1 = document.getElementById("item1");
+var item2 = document.getElementById("item2");
+var item3 = document.getElementById("item3");
+var item4 = document.getElementById("item4");
 
-greece.addEventListener("click", function(){
-    greeceinfo.innerHTML = "Go to Greece today to enjoy a beautiful vacation filled with breathtaking views and interesting history behind the centuries old country. There are many activities that you can do there such as boating or cruising to different islands on Greece, sightseeing Delphi, one of the most important archeological sites in Greek history. You can also tour Athens, Greece's beloved capital filled with amazing history and stories to share. Last, but not least you must visit, Santorini Architecture to see whitewashed homes and blue-domed churches next to thee deep, blue ocean. Visit Greece to enjoy the beautiful beaches and see the history of Greece today!<br><br> Location: Southeastern Europe<br>Capital: Athens<br>Popuation: 11.03million<br>Activities: Beaches, Hiking, Sightseeing, Museums, Boating, Swimming";
+var info1 = document.getElementById("info1");
+var info2 = document.getElementById("info2");
+var info3 = document.getElementById("info3");
+var info4 = document.getElementById("info4");
+
+item1.addEventListener("click", function(){
+    info1.innerHTML = "Greece";
+    info2.innerHTML = "Go to Greece today to enjoy a beautiful vacation filled with breathtaking views and interesting history behind the centuries old country. There are many activities that you can do there such as boating or cruising to different islands on Greece, sightseeing Delphi, one of the most important archeological sites in Greek history. You can also tour Athens, Greece's beloved capital filled with amazing history and stories to share. Last, but not least you must visit, Santorini Architecture to see whitewashed homes and blue-domed churches next to thee deep, blue ocean. Visit Greece to enjoy the beautiful beaches and see the history of Greece today!<br><br> Location: Southeastern Europe<br>Capital: Athens<br>Popuation: 11.03million<br>Activities: Beaches, Hiking, Sightseeing, Museums, Boating, Swimming";
 });
             
-israel.addEventListener("click", function(){
-    israelinfo.innerHTML = "Go to Israel today to experience unique tradition, religion, and history, but also, increasingly, of modern culture and heritage. Visit the alluring landscape Israel has to offer such as Galilee or Negev Desert. Surrounded by the beautiful scenery of the Negev Desert, there lies the Dead Sea. A fun fact about the Dead Sea is that it is the lowest place on Earth and you can enjoy a relaxing swim in it as well! COme to the most historical and religious country in the world today and experience new cultures, religions, and new ideas!<br><br> Location: Middle Eastern city west of the Dead Sea<br>Capital: Jerusalem<br>Popuation: 809,112<br>Activities: Cycling, Jeep Tours, ATVs &Quad Runners, Horseback Riding, Rafting & Kayaking, Paragliding, Scuba Diving, Swim with Dolphins, Camel Riding";
+item2.addEventListener("click", function(){
+    info1.innerHTML = "Israel";
+    info2.innerHTML = "Go to Israel today to experience unique tradition, religion, and history, but also, increasingly, of modern culture and heritage. Visit the alluring landscape Israel has to offer such as Galilee or Negev Desert. Surrounded by the beautiful scenery of the Negev Desert, there lies the Dead Sea. A fun fact about the Dead Sea is that it is the lowest place on Earth and you can enjoy a relaxing swim in it as well! Come to the most historical and religious country in the world today and experience new cultures, religions, and new ideas!<br><br> Location: Middle Eastern city west of the Dead Sea<br>Capital: Jerusalem<br>Popuation: 809,112<br>Activities: Cycling, Jeep Tours, ATVs &Quad Runners, Horseback Riding, Rafting & Kayaking, Paragliding, Scuba Diving, Swim with Dolphins, Camel Riding";
 });
 
-newyork.addEventListener("click", function(){
-    newyorkinfo.innerHTML = "The city that never sleeps is one place you don't want to miss. New York City is one of the most diverse, cultural cities in the world. You can enjoy authentic, savory dishes and of course delicious pizza. After eating, you must visit famous attractions such as the Statue of Liberty, Empire State Building, Central Park, Metropolitan Museum of of Art, and Times Square. Don't forget to buy tickets and watch an entertaining and exquist Broadway show that consist of talented singers and wonderful dancers. Visit New York city for a once in a life time experience in this special city today!<br><br> Location: Eastern part of the US<br>Capital: New York City<br>Popuation: 8.406 million<br>Activities: Tour New York, Try all kinds of different cultural food, Walk in Central Park, Broadway Shows, Sightseeing, Museums, Shopping";
+item3.addEventListener("click", function(){
+    info1.innerHTML = "New York";
+    info2.innerHTML = "The city that never sleeps is one place you don't want to miss. New York City is one of the most diverse, cultural cities in the world. You can enjoy authentic, savory dishes and of course delicious pizza. After eating, you must visit famous attractions such as the Statue of Liberty, Empire State Building, Central Park, Metropolitan Museum of of Art, and Times Square. Don't forget to buy tickets and watch an entertaining and exquist Broadway show that consist of talented singers and wonderful dancers. Visit New York for a once in a life time experience in this special city!<br><br> Location: Eastern part of the US<br>Capital: New York City<br>Popuation: 8.406 million<br>Activities: Tour New York, Try all kinds of different cultural food, Walk in Central Park, Broadway Shows, Sightseeing, Museums, Shopping";
 });
 
-barcelona.addEventListener("click", function(){
-    barcelonainfo.innerHTML = "Enjoy a relaxing and enchanting city at Barcelona where you will experience the histroy, culture, and amazing architecture Barcelona has to offer. There are many different activities in Barcelon for everyone for example, museums, there are endless amounts of museums, having Barcelon one of the oldest cities in the world. Although, if you are more of a party person, you will enjoy some of the best nightclubs in the owrld such as Opium, Shoko, Pacha and Catwalk! The third activity, which is my favorite, is a realxing day at the beach. Barcelona is home to some of the best beaches in Europe. Last, and definitely not least is the food because who doesn't love food? You will be able to find fresh fish and many different cultured foods there to try. COme to Barcelona today and get the beautiful vacation you deserve!<br><br> Location: Europe's Iberian Peninsula<br> Capital: Madrid<br> Popuation: 46.77 million<br> Activities: Sightseeing, Tours, Boating, Wine Tasting, Concerts, Shopping, Water Sports, Amusment Parkd, Spas";
+item4.addEventListener("click", function(){
+    info1.innerHTML = "Barcelona";
+    info2.innerHTML = "Enjoy a relaxing and enchanting city at Barcelona where you will experience the histroy, culture, and amazing architecture Barcelona has to offer. There are many different activities in Barcelon for everyone for example, museums, there are endless amounts of museums, having Barcelon one of the oldest cities in the world. Although, if you are more of a party person, you will enjoy some of the best nightclubs in the owrld such as Opium, Shoko, Pacha and Catwalk! The third activity, which is my favorite, is a realxing day at the beach. Barcelona is home to some of the best beaches in Europe. Last, and definitely not least is the food because who doesn't love food? You will be able to find fresh fish and many different cultured foods there to try. Come to Barcelona today and get the beautiful vacation you deserve!<br><br> Location: Europe's Iberian Peninsula<br> Capital: Madrid<br> Popuation: 46.77 million<br> Activities: Sightseeing, Tours, Boating, Wine Tasting, Concerts, Shopping, Water Sports, Amusment Parkd, Spas";
 });
