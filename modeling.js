07d490ab3e749923bcd21dec2667859061355fec
mikaela123 <mconsunji.sh@mysummitps.org>
2016-03-29 22:23:13 +0000

    update

--- modeling.js
+++ modeling.js
@@ -1,1 +1,8 @@
 // JavaScript File
+var pink_h= document.getElementById("pink_h");
+
+var pinkhat= document.getElementById("pinkhat");
+
+pink_h.addEventListener("click", function(){
+    pinkhat.innerHTML = pinkhat
+    
